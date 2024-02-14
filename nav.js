bacc00b4ec3c93c64c658bdadba6c1ee6cb89d6d
Rahman <rahmansaif.413@gmail.com>
2024-02-14 12:23:06 +0600

    refactored js code to add more efficiency

--- nav.js
+++ nav.js
@@ -4,9 +4,10 @@ console.log(sectionClass);
 // console.log(liContainers.classList);
 // console.log(liContainers.classList.contains("display-none"))
 sectionClass.addEventListener("click",function(){
-    if(liContainers.classList.contains("display-none")){
-        liContainers.classList.add("display-show")
-    }else{
-        liContainers.classList.add("display-none")
-    }
+    // if(liContainers.classList.contains("display-none")){
+    //     liContainers.classList.add("display-show")
+    // }else{
+    //     liContainers.classList.add("display-none")
+    // }
+    liContainers.classList.toggle("display-show");
 })
